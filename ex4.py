e466d3002f25445965d6374dffa1153bbb1ba75d
Douglas Freitas <freitas28douglas@gmail.com>
2022-03-13 20:31:39 -0300

    up

--- ex4.py
+++ ex4.py
@@ -1,4 +1,4 @@
 dias = int(input('Quantos dias foi alugado? '))
 km = float(input('Quantos km rodados? '))
 total = (dias * 60) + (km * 0.15)
-print('O total a pagar é de R${:2f}'.format(total))
+print('O total a pagar é de R${:.2f}'.format(total))
